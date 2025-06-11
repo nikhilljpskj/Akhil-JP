e358dbdd37d652215846c7f4853365b8a0e0ce01
Nikhil Prakash <115891140+nikhilljpskj@users.noreply.github.com>
2025-06-11 20:34:29 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ export const headerData = {
     name: 'AKHIL JP',
     title: "Hello! I am",
     desciption: "Welcome to my profile!!!  Sub Engineer from Kerala. I'm Akhil Prakash, working with Kerala State Electricity Board.",
-    image: 'https://drive.google.com/file/d/1XJxPgCVhkUS4sYh7lfaGmnXbRsK2pKnW/view?usp=sharing',
-    imagebw: 'https://drive.google.com/file/d/1vWbxDApKYSwPulAymr1bfxlDxPGXlkUu/view?usp=sharing',
+    image: 'https://images.unsplash.com/photo-1749654208781-a798e4fce499?q=80&w=1984&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imagebw: 'https://images.unsplash.com/photo-1749654208683-b9579d9e3724?q=80&w=1984&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     resumePdf: 'https://drive.google.com/file/d/1AU-RqKLrM5mMXzKPt69mDhchfq7vJd9u/view?usp=sharing'
 }
