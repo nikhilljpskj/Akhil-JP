6c2296fbf326485c212d510b4868f77c9198e255
nikhilljpskj <nikhiljp.skj@gmail.com>
2026-02-09 09:43:01 +0530

    Fix Next.js version and package index and other updates

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages";
+import Index from "./pages/index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
