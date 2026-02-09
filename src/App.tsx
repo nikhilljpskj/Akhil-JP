0fbd88269bc0d1a3d7e136075718159de7858e5a
nikhilljpskj <nikhiljp.skj@gmail.com>
2026-02-09 10:00:38 +0530

    Fix Vite build: correct pages path and CSS import order

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/index";
 import NotFound from "./pages/NotFound";
 
+
 const queryClient = new QueryClient();
 
 const App = () => (
