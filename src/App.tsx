edbb3e71a8c210c1361441d2590628a550b65698
nikhilljpskj <nikhiljp.skj@gmail.com>
2026-02-09 09:34:37 +0530

    Fix Next.js version and package index and other updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
